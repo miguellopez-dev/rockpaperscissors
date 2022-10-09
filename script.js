af2fb241841f84b0d0868298b0b040f2ec9b4140
Miguel Lopez <mlopezt.1215@gmail.com>
2022-10-08 17:16:40 -0700

    Finish dialouge box, format code

--- script.js
+++ script.js
@@ -1,113 +1,92 @@
-let PLAYER_SCORE = 0;
-let COMPUTER_SCORE = 0;
-
-const resetYes = document.querySelector('.reset__choice-yes');
-const resetContainer = document.querySelector('.reset');
-const resetResult = document.querySelector('.reset__content h2');
+let PLAYER_SCORE = 4;
+let COMPUTER_SCORE = 4;
 
 //Player takes their pick
-function display(text){ 
+function DISPLAY(text) {
 	document.querySelector('.dialouge__box p').innerHTML = text;
 }
 
+//Starts the game when player picks a choice and calls computers turn
 const picks = document.querySelectorAll('.picks__choice');
 picks.forEach((element) => {
 	element.addEventListener('click', () => {
-		let playerChoice = element.classList[1]; 
-		display(`You chose ${playerChoice}!`)
+		let playerChoice = element.classList[1];
+		DISPLAY(`You chose ${playerChoice}!`)
 		setTimeout(() => {
-			display('Computer is choosing...')
+			DISPLAY('Computer is choosing...')
 		}, 1000);
 		computerPlay(playerChoice);
 	});
 });
 
+//Computer randomly picks then calls function to check who wins
 function computerPlay(playerChoice) {
 	const choice = ['rock', 'paper', 'scissors'];
 	const computerChoice = choice[Math.floor(Math.random() * 3)];
 	setTimeout(() => {
-		display(`Computer chose ${computerChoice}!`)
+		DISPLAY(`Computer chose ${computerChoice}!`)
 	}, 1866);
-	playRound(playerChoice, computerChoice)
+	checkWinner(playerChoice, computerChoice)
 }
 
-function playRound(playerChoice, computerChoice){
+//Checks winner then calls a function to update the score
+function checkWinner(playerChoice, computerChoice) {
 	setTimeout(() => {
-		if (playerChoice === computerChoice){
-			display(`It's a tie!`);
-		}else if(playerChoice === 'rock' && computerChoice === 'scissors'
+		if (playerChoice === computerChoice) {
+			DISPLAY(`It's a tie!`);
+		} else if (playerChoice === 'rock' && computerChoice === 'scissors'
 			|| playerChoice === 'scissors' && computerChoice === 'paper'
-			|| playerChoice === 'paper' && computerChoice === 'rock'){
-				display('You Win!');
-				scorePlayer();
-				
+			|| playerChoice === 'paper' && computerChoice === 'rock') {
+			DISPLAY('You Win!');
+			scorePlayer();
+
 		} else {
-			display('Computer Wins!');
+			DISPLAY('Computer Wins!');
 			scoreComputer();
 		}
+
 	}, 2780);
-	
-	if(PLAYER_SCORE === 5 || COMPUTER_SCORE === 5){
-		reset();
-	}
 }
-function updateScore(person, score){
+
+//Updates score and if score is 5, pop up appears to reset game
+function updateScore(person, score) {
 	document.querySelector(`.${person} p`).innerHTML = score;
 }
-function scorePlayer(){
+function scorePlayer() {
 	++PLAYER_SCORE;
 	updateScore('score__player', PLAYER_SCORE);
+	if (PLAYER_SCORE === 5) {
+		RESET_CONTAINER_ADD('You Win!');
+	}
 }
 
-function scoreComputer(){
+function scoreComputer() {
 	++COMPUTER_SCORE;
 	updateScore('score__computer', COMPUTER_SCORE);
+	if (COMPUTER_SCORE === 5) {
+		RESET_CONTAINER_ADD('Computer Wins!');
+	}
+
 }
 
-function game() {
-	// The computer will randomly select one of the choices.
-	
-	const computerSelection = computerPlay();
 
-	playRound(playerSelection, computerSelection);
-
-	if (answer.includes('win!')) {
-		playerScore++;
-		containerScorePlayer.innerHTML = playerScore;
-	} else if (answer.includes('lose!')) {
-		computerScore++;
-		containerScoreComp.innerHTML = computerScore;
-	}
-	// if player reaches 5 points banner pops up and prevents clicking any where else
-	if (playerScore >= 5) {
-		resetContainer.classList.add('reset__fade-in');
-		if (resetContainer.classList.contains('start')) {
-			resetContainer.classList.remove('start');
-		} else {
-			resetContainer.classList.remove('reset__fade-out');
-		}
-		resetResult.textContent = 'You Win!';
-		// if computer reaches 5 points banner pops up and prevents clicking any where else
-	} else if (computerScore >= 5) {
-		resetContainer.classList.add('reset__fade-in');
-		if (resetContainer.classList.contains('start')) {
-			resetContainer.classList.remove('start');
-		} else {
-			resetContainer.classList.remove('reset__fade-out');
-		}
-
-		resetResult.textContent = 'You Lose!';
-	}
+//Makes pop up appear by adding fade-in class and removing the start class
+function RESET_CONTAINER_ADD(result) {
+	document.querySelector('.reset').classList.add(`reset__fade-in`);
+	document.querySelector('.reset').classList.remove('start');
+	document.querySelector('.reset__content h2').innerHTML = result;
 }
 
 // Reset all info and removes reset banner
 function reset() {
-	playerScore = 0;
-	computerScore = 0;
-	containerScorePlayer.innerHTML = playerScore;
-	containerScoreComp.innerHTML = computerScore;
+	const resetContainer = document.querySelector('.reset');
+	PLAYER_SCORE = 0;
+	COMPUTER_SCORE = 0;
+	updateScore('score__player', 0);
+	updateScore('score__computer', 0);
 	resetContainer.classList.remove('reset__fade-in');
 	resetContainer.classList.add('reset__fade-out');
 }
 
+const resetYes = document.querySelector('.reset__choice-yes');
 resetYes.addEventListener('click', reset);
